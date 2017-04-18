6dc234d556b5c1ccd6e685145d91eaf6e80dbd3d
lockyang <651045756@qq.com>
2017-04-18 19:32:10 +0800

    React + webpack2

---
+++ React/app/components/Greeter.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+import React, { Component } from 'react';
+import './Greeter.css'
+class Greeter extends Component {
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return <div className="tab">123</div>
+  }
+}
+
+export default Greeter
--- React/webpack.config.js
+++ React/webpack.config.js
@@ -1,64 +1,145 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var merge = require('webpack-merge');
+var cssnext = require('cssnext');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+// var mixins = require('./app/public/css/mixins.js');
 
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
-const ROOT = process.cwd();
+var TARGET = process.env.npm_lifecycle_event;
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = TARGET === 'build' ?
+       path.resolve(ROOT_PATH, 'build') :
+       path.resolve(ROOT_PATH, 'build-dev');
 
-module.exports = {
-  entry: __dirname + '/src/index.js',
-  output: {
-    path: __dirname + '/dist',
-    filename: 'bundle.js'
+var common = {
+  entry: [
+    require.resolve('react-dev-utils/webpackHotDevClient'),
+    APP_PATH
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx']
   },
-  devtool: 'source-map',
-  devServer: {
-    watchOptions: {
-      ignored: /node_modules/
-    },
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    port: '8082',
+  output: {
+    path: BUILD_PATH,
+    filename: 'bundle.[hash].min.js',
+    publicPath: '/',
+    chunkFilename: "[name].[chunkhash].min.js"
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: [/node_modules/],
-        use: [{
-          loader: 'babel-loader'
-        }],
-      },
+    rules:[
       {
         test: /\.css$/,
-        use: [
-          {
-            loader: 'style-loader'
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          }
-        ]
-      },
-      {
+        loader: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          loader: 'css-loader!postcss-loader',
+        })
+      }, {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }, {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000
+        }
+      }, {
         test: /\.(ttf|eot|svg|otf)(\?v=\d(\.\d){2})?$/,
-        use: 'file-loader'
+        loader: 'file'
+      }, {
+        test: /\.(js|jsx)?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        include: path.resolve(ROOT_PATH, 'app')
       },
-      {
-        test: /\.woff(2)?(\?v=\d(\.\d){2})?$/,
-        use: 'url?limit=10000&minetype=application/font-woff'
-      }
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      inject: 'body',
-      template: 'index.html',
-      hash: true
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        context: __dirname,
+        postcss: function(webpack) {
+          return [
+            require('postcss-import')(),
+            require('postcss-url')(),
+            require('postcss-px2rem')({remUnit: 75}),
+            require("postcss-mixins")(),
+            require('postcss-cssnext')({
+              browsers: [
+                '>1%',
+                'last 4 versions',
+                'Firefox ESR',
+                'not ie < 9',
+              ]
+            }),
+            require("postcss-nested")()
+          ]
+        }
+      },
+
     }),
+
+    new HtmlWebpackPlugin({
+      title: '掉钱眼儿',
+      favicon: '',
+      template: 'template-index.html',
+      inject: 'body'
+    })
   ]
+};
+
+if (TARGET === 'start' || TARGET === 'dev') {
+  module.exports = merge(common, {
+    devtool: '#cheap-module-eval-source-map',
+    module: {
+      rules: [{
+          test: /\.(js|jsx)$/,
+          loader: 'eslint-loader',
+          enforce: 'pre',
+          include: path.resolve(ROOT_PATH, 'app'),
+          options: {
+            formatter: require('eslint-friendly-formatter')
+          }
+        }
+      ],
+    },
+
+    plugins: [
+      // CopyWebpackPlugin([
+      //   {from: APP_PATH + '/utils/godcss.js'}
+      // ])
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"development"'
+      }),
+
+      // new ExtractTextPlugin('bundle.[contenthash].min.css'),
+
+      new webpack.HotModuleReplacementPlugin(),
+    ]
+  })
+}
+
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {
+    devtool: '#cheap-module-source-map',
+    plugins: [
+      new ExtractTextPlugin('bundle.[contenthash].min.css'),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': '"production"'
+      }),
+      new OccurrenceOrderPlugin(),
+      new UglifyJsPlugin({
+        compress: {
+          warnings: true
+        }
+      })
+    ]
+  })
 }
